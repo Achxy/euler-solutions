d282acbe596e9dc0c3f6465767247e7a3521ec11
Achxy <74144790+Achxy@users.noreply.github.com>
2022-04-23 01:54:12 +0530

    benchmark and pyproject

---
+++ src/benchmark.py
@@ -0,0 +1,17 @@
+from time import perf_counter
+from typing import Callable, ParamSpec, TypeVar
+
+R = TypeVar("R")
+P = ParamSpec("P")
+
+
+def measure(func: Callable[P, R]) -> Callable[P, R]:
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+        start: float = perf_counter()
+        result: R = func(*args, **kwargs)
+        print(
+            f"{func.__name__} took {(perf_counter() - start) * 1000} milliseconds to be completed"
+        )
+        return result
+
+    return wrapper
