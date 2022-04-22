db999f6743d02f3b60600284ab6a62da58cb97bd
Achxy <74144790+Achxy@users.noreply.github.com>
2022-04-23 02:01:20 +0530

    problem 1

---
+++ src/problem_1.py
@@ -0,0 +1,18 @@
+from benchmark import measure
+
+
+def _summation_of_progression(leng, num):
+    k = leng // num
+    return k * ((num + (num * k)) / 2)
+
+
+@measure
+def problem_1(length):
+    length -= 1
+    return (
+        _summation_of_progression(length, 3) + _summation_of_progression(length, 5)
+    ) - _summation_of_progression(length, 15)
+
+
+if __name__ == "__main__":
+    print(problem_1(1000))
