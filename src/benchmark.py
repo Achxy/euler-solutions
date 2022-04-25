bcf01fb6514d24db5dcf21227bf739c69f5042e5
Achxy <74144790+Achxy@users.noreply.github.com>
2022-04-26 03:09:55 +0530

    benchmarks now show returned result

--- src/benchmark.py
+++ src/benchmark.py
@@ -9,7 +9,12 @@ def measure(func: Callable[P, R]) -> Callable[P, R]:
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
         start: float = perf_counter()
         result: R = func(*args, **kwargs)
-        print(f"{func.__name__} took {(perf_counter() - start) * 1000} milliseconds to be completed")
+        print(
+            (
+                f"{func.__name__} took {(perf_counter() - start) * 1000} "
+                f"milliseconds to be completed and returned {result}"
+            )
+        )
         return result
 
     return wrapper
