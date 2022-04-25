b9d4331424a5dfee939a052e2c687b3a185da0be
Achxy <74144790+Achxy@users.noreply.github.com>
2022-04-26 03:09:30 +0530

    fixed typo in function named (renamed to `6`)

--- src/problem_6.py
+++ src/problem_6.py
@@ -2,9 +2,9 @@ from benchmark import measure
 
 
 @measure
-def problem_3(lim=100):
+def problem_6(lim=100):
     return int(((lim * (lim + 1) // 2) ** 2) - (lim + 1) * lim / 6 * (2 * lim + 1))
 
 
 if __name__ == "__main__":
-    print(problem_3(100))
+    print(problem_6(100))
