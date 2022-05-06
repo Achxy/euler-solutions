3fab0adf8573ef2777576b256131799b9590675f
Achxy <74144790+Achxy@users.noreply.github.com>
2022-05-07 00:17:50 +0530

    adapt `Benchmarked`

--- src/__main__.py
+++ src/__main__.py
@@ -1,14 +1,18 @@
 from __future__ import annotations
 
 import importlib
-import time
+from collections.abc import Generator, Iterable
 from pathlib import Path
-from typing import Callable, TypeAlias
+from typing import TypeAlias, TypeVar
 
-FnMap: TypeAlias = dict[str, Callable[..., int]]
+from benchmark import Benchmarked
+
+BenchmarkMap: TypeAlias = dict[str, Benchmarked]
+IGNORE: tuple[str, ...] = ("benchmark.py", "__main__.py")
+T = TypeVar("T", bound=Benchmarked)
 
 
-def get_all_files(directory: Path, ignore=("benchmark.py", "__main__.py")) -> list[Path]:
+def get_all_files(directory: Path, ignore=IGNORE) -> list[Path]:
     """
     Returns a list of all files in the given directory.
     The mentioned in `ignore` are ignored from the result
@@ -24,7 +28,7 @@ def get_all_files(directory: Path, ignore=("benchmark.py", "__main__.py")) -> li
     return files
 
 
-def load_all_files_in_directory(directory: Path) -> FnMap:
+def load_all_files_in_directory(directory: Path) -> BenchmarkMap:
     """
     Loads all files in the given directory and returns a map of the loaded functions
     The value of the map is the function that was loaded with same name as the file
@@ -34,30 +38,41 @@ def load_all_files_in_directory(directory: Path) -> FnMap:
         directory (Path): The directory to search for files
 
     Returns:
-        FnMap: A map of problems to their functions
+        BenchmarkMap: A map of problems to their benchmarked functions
     """
     files: list[Path] = get_all_files(directory)
-    functions: FnMap = {}
+    benchmap: BenchmarkMap = {}
     for file in files:
         module = importlib.import_module(file.stem)
-        functions[file.stem] = module.__dict__[file.stem]
-    return functions
+        benchmap[file.stem] = module.__dict__[file.stem]
+    return benchmap
+
+
+def as_completed(functions: Iterable[T]) -> Generator[T, None, None]:
+    """
+    Returns a generator that yields the functions in the given iterable in order
+    This calls the constituent functions and returns it so that elapsed time can be calculated
+    args:
+        functions (Iterable[T]): The functions to yield
+
+    Returns:
+        Generator[T, None, None]: A generator that yields the functions in the given iterable in order
+    """
+    for fn in functions:
+        fn()
+        yield fn
 
 
 def start() -> None:
     """
     Entry point for executing all the programs in the directory
     """
-    s_time: float = time.perf_counter()
-    functions: FnMap = load_all_files_in_directory(Path(__file__).parent)
-    for fn in functions.values():
-        fn()  # Results should be printed because of the `measure` decorator in the benchmark.py file
-    print(
-        (
-            f"Total time for {len(functions)} problems to be "
-            f"completed: {(time.perf_counter() - s_time) * 1000} milliseconds"
-        )
-    )
+    functions: BenchmarkMap = load_all_files_in_directory(Path(__file__).parent)
+    elapsed: float = 0
+    problem_count: int = len(functions)
+    for fn in as_completed(functions.values()):
+        elapsed += fn.elapsed()
+    print(f"Total time taken for all {problem_count} problems: {elapsed} milliseconds")
 
 
 if __name__ == "__main__":
