8b99cef695a63bf14f1189a02d88d3547c7f2008
Achxy <74144790+Achxy@users.noreply.github.com>
2022-06-12 13:15:50 +0530

    set _get_data func as kwarg

--- python/problem_13.py
+++ python/problem_13.py
@@ -4,16 +4,15 @@ from pathlib import Path
 PATH = Path(__file__).parent.parent / "data" / "p013_numbers.txt"
 
 
-@Benchmarked
-def problem_13(data):
-    return str(sum(data))[:10]
-
-
-def _get_data(path):
+def _get_data(path=PATH):
     with open(path) as f:
         return map(int, f.read().splitlines())
 
 
+@Benchmarked
+def problem_13(data=_get_data()):
+    return str(sum(data))[:10]
+
+
 if __name__ == "__main__":
-    data = _get_data(PATH)
-    problem_13(data)
+    problem_13()
