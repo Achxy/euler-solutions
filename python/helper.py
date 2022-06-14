ab300aafcc21e362b6970d2785b766ab132df471
Achxy <74144790+Achxy@users.noreply.github.com>
2022-06-14 19:49:47 +0530

    overwrite `print` from rich or use builtin `print`

--- python/helper.py
+++ python/helper.py
@@ -1,8 +1,17 @@
 from pathlib import Path
 
-__all__: tuple[str] = ("form_path",)
+__all__: tuple[str, ...] = (
+    "form_path",
+    "print",
+)
 
 PATH: Path = Path(__file__).parent.parent / "data"
+
+
+try:
+    from rich import print
+except ImportError:
+    print = __builtins__.print
 
 
 def form_path(filename) -> Path:
