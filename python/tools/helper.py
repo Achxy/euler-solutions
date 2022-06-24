a0247c9dda742089e6e89f79435599a4a2a85a8c
Achxy <74144790+Achxy@users.noreply.github.com>
2022-06-24 22:26:45 +0530

    index `parents` property

--- python/tools/helper.py
+++ python/tools/helper.py
@@ -4,7 +4,7 @@ from typing import Final
 from .typeshack import All
 
 __all__: All = ("form_path",)
-PATH: Final[Path] = Path(__file__).parent.parent.parent / "data"
+PATH: Final[Path] = Path(__file__).parents[2] / "data"
 
 
 def form_path(filename: str) -> Path:
