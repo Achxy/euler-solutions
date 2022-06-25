8f014d965e271ddfc59511f99cf49dcc50a488cb
Achxy <74144790+Achxy@users.noreply.github.com>
2022-06-25 23:19:58 +0530

    `clean_whitespaces` function

--- python/tools/helper.py
+++ python/tools/helper.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from re import sub
 from typing import Final
 
 from .typeshack import All
@@ -20,3 +21,17 @@ def form_path(filename: str) -> Path:
         Path: The path formed by concatenation
     """
     return PATH / filename
+
+
+def clean_whitespaces(string: str) -> str:
+    """
+    Removes whitespaces characters such as new line, return carriage, form feed
+    et cetera from a given string
+
+    Args:
+        string (str): The string to remove the whitespace from
+
+    Returns:
+        str: The new string returned with whitespaces removed
+    """
+    return sub(r"\s+", "", string)
