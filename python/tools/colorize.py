c653934c4cc09f4e4440a8c82c56f2d0668e4015
Achxy <74144790+Achxy@users.noreply.github.com>
2022-06-25 18:21:45 +0530

    implement colors than using colorama

--- python/tools/colorize.py
+++ python/tools/colorize.py
@@ -1,14 +1,19 @@
-from typing import Any
+_CSI = "\033["
+_M = "m"
 
-from colorama import Fore, Style
+RED = _CSI + "31" + _M
+GREEN = _CSI + "32" + _M
+YELLOW = _CSI + "33" + _M
+RESET = _CSI + "0" + _M
+
 
 COLOR_RANGE: dict[range, str] = {
-    range(0, 140): Fore.GREEN,
-    range(140, 300): Fore.YELLOW,
+    range(0, 140): GREEN,
+    range(140, 300): YELLOW,
 }
 
 
-def colorize(obj: Any, color) -> str:
+def colorize(obj: object, color: str) -> str:
     """
     Takes in an object of any instance, calls __str__
     on it and then concatenates ansi escape sequence to it
@@ -21,10 +26,10 @@ def colorize(obj: Any, color) -> str:
     Returns:
         str: Concatenated result
     """
-    return color + str(obj) + Style.RESET_ALL
+    return color + str(obj) + RESET
 
 
-def time_format(number: float, fallback: str = Fore.RED) -> str:
+def time_format(number: float, fallback: str = RED) -> str:
     """
     Used for pretty formatting time
     fallback is used if `number` argument is not present in the color
